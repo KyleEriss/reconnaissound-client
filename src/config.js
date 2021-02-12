f9e0e4ecdebf7478f9c64c7abea89eb53f6d5ad6
Kyle Eriss <kyle.eriss@gmail.com>
2021-02-11 19:35:45 -0500

    changes

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-  API_ENDPOINT: 'https://hidden-temple-45821.herokuapp.com',
+  API_ENDPOINT: 'https://serene-bayou-40176.herokuapp.com',
   API_KEY: process.env.REACT_APP_API_KEY,
   TOKEN_KEY: 'reconnaissound-client-auth-token',
 };
