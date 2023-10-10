6cac03470c8131b0cee7a2b09235128d18f4152e
Kyle Eriss <keriss@mooveguru.com>
2023-10-10 15:58:20 -0400

    Updating API_ENDPOINT and dependencies

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-  API_ENDPOINT: 'https://serene-bayou-40176.herokuapp.com',
+  API_ENDPOINT: 'https://glacial-oasis-57034-a2cf2c95e667.herokuapp.com',
   API_KEY: process.env.REACT_APP_API_KEY,
   TOKEN_KEY: 'reconnaissound-client-auth-token',
 };
